1fae9a69a8357a7bac7273ed7f9594389510864d
Sergio Mauricio Nuñez <106033664+iamsergio7@users.noreply.github.com>
2022-12-29 18:15:23 -0400

    testeando puerto a railway en app

--- src/config.js
+++ src/config.js
@@ -1,19 +1,29 @@
 require('dotenv').config();
 
 module.exports = {
-    app:{
-        port: process.env.PORT || 4000,
+    app: {
+        PORT: process.env.PORT || 4000,
+        DB_HOST : process.env.DB_HOST || "localhost",
+        DB_USER : process.env.DB_USER || "root",
+        DB_PASSWORD : process.env.DB_PASSWORD || "password",
+        DB_NAME : process.env.DB_NAME || "panaderia",
+        DB_PORT : process.env.DB_PORT || 3306
     },
-    jwt:{
+    jwt: {
         secret: process.env.JWT_SECRET || 'mauri007'
     },
 
-    mysql:{
+    mysql: {
         DB_HOST: process.env.MYSQL_HOST || 'localhost',
         DB_USER: process.env.MYSQL_USER || 'root',
         DB_PASSWORD: process.env.MYSQL_PASSWORD || '',
         DB_NAME: process.env.MYSQL_DATABASE || 'panaderia',
         DB_PORT: process.env.MYSQL_PORT || 3306
+
+        // DB_HOST : process.env.DB_HOST || "localhost",
+        // DB_USER : process.env.DB_USER || "root",
+        // DB_PASSWORD : process.env.DB_PASSWORD || "password",
+        // DB_NAME : process.env.DB_NAME || "panaderia",
+        // DB_PORT : process.env.DB_PORT || 3310
     }
 }
-    
